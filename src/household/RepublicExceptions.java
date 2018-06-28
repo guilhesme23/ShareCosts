79ea55bbc430e53c76cebd36746a5317dc866034
viniciused26 <viniciusoliv26@gmail.com>
2018-06-27 23:20:57 -0300

    Criando nova classe de exceções de dados de usuário

---
+++ src/household/RepublicExceptions.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package household;
+
+/**
+ *
+ * @author vinicius
+ */
+public class RepublicExceptions extends Exception{
+    
+}
